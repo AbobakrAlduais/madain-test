9d8e7bd2c8766629db59b0468a00a065e566e051
AbobakrAlduais <aalduais@gmail.com>
2017-12-12 10:48:32 +0200

    Add gender

--- public/js/loginCtrl.js
+++ public/js/loginCtrl.js
@@ -3,7 +3,7 @@ loginApp.controller('loginCtrl',['$scope','$http',($scope,$http) => {
 
     $scope.login= () =>
     {
-        $http.post('/api/login', {email:$scope.email , password:$scope.password}).then(r => {
+        $http.post('/api/login', {email:$scope.email , password:$scope.password,gender:$scope.gender}).then(r => {
             $scope.user = r.data;
         }, e => {
             $scope.errorMessage = e.data.message;
--- server.js
+++ server.js
@@ -24,19 +24,21 @@ app.post('/api/login', function (req, res) {
         if(req.body.email == '123@123.123'){
 
             if(req.body.password == '123123') {
+              console.log( req.body.gender) 
                 var user ={
                     name:"Alex Jones"
                     , email:req.body.email
                     , password:req.body.password
                     , profilePic:"http://lorempixel.com/500/500/people/"
+                    , gender:req.body.gender
                 };
                 res.send(200, user);
             }
-            else
-                res.send(400,{message:'hey lady, you sent me the wrong password.'});
+        elseif(user.gender == 'female')
+             { res.send(400,{message:'hey lady, you sent me the wrong password.'});}
 
-        }else
-            res.send(400,{message:'hey man, you sent me the wrong email.'});
+        }else if(user.gender == 'male')
+            {res.send(400,{message:'hey man, you sent me the wrong email.'});}
 
     }
     else
